a0e2f6b871ccf8e332dd7b9fa7df7c91adeb6f6c
anzehao <anzehao@ixianlai.com>
2020-03-27 18:04:29 +0800

    new: first commit

---
+++ 2jobs/TestingJobs.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Burst;
+using UnityEngine.Jobs;
+
+public class TestingJobs : MonoBehaviour
+{
+    public bool useJobs;
+    
+    public Transform pfZombie;
+    private List<Zombie> zombieList;
+
+    public class Zombie {
+        public Transform transform;
+        public float moveY;
+    }
+
+    private void Start() {
+        zombieList = new List<Zombie>();
+        for (int i = 0; i < 1000; i++)
+        {
+            Transform zombieTransform = Instantiate(pfZombie, new Vector3(UnityEngine.Random.Range(-8f,8f), UnityEngine.Random.Range(-5f,5f)), Quaternion.identity);
+            zombieList.Add(new Zombie {
+                transform = zombieTransform,
+                moveY = UnityEngine.Random.Range(1f,2f)
+            });
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        if (useJobs)
+        {
+            // NativeArray<float3> positionArray = new NativeArray<float3>(zombieList.Count, Allocator.TempJob);
+            NativeArray<float> moveYArray = new NativeArray<float>(zombieList.Count, Allocator.TempJob);
+
+            TransformAccessArray transformAccessArray = new TransformAccessArray(zombieList.Count);
+
+            for (int i = 0; i < zombieList.Count; i++)
+            {
+                // positionArray[i] = zombieList[i].transform.position;
+                moveYArray[i] = zombieList[i].moveY;
+                transformAccessArray.Add(zombieList[i].transform);
+            }
+
+            /*
+            ReallyToughParallelJob reallyToughParallelJob = new ReallyToughParallelJob {
+                deltaTime = Time.deltaTime,
+                positionArray = positionArray,
+                moveYArray = moveYArray
+            };
+
+            JobHandle jobHandle = reallyToughParallelJob.Schedule(zombieList.Count, 10);
+            jobHandle.Complete();
+            */
+
+            ReallyToughParallelJobTransforms reallyToughParallelJobTransforms = new ReallyToughParallelJobTransforms {
+                deltaTime = Time.deltaTime,
+                moveYArray = moveYArray
+            };
+            JobHandle jobHandle = reallyToughParallelJobTransforms.Schedule(transformAccessArray);
+            jobHandle.Complete();
+
+            for (int i = 0; i < zombieList.Count; i++)
+            {
+                // zombieList[i].transform.position = positionArray[i];
+                zombieList[i].moveY = moveYArray[i];
+            }
+
+            // positionArray.Dispose();
+            moveYArray.Dispose();
+
+            transformAccessArray.Dispose();
+
+        }
+        else {
+            foreach (var zombie in zombieList)
+            {
+                zombie.transform.position += new Vector3(0, zombie.moveY * Time.deltaTime);
+                if (zombie.transform.position.y > 5f)
+                {
+                    zombie.moveY = -math.abs(zombie.moveY);
+                }
+                if (zombie.transform.position.y < -5f)
+                {
+                    zombie.moveY = +math.abs(zombie.moveY);
+                }
+                float value = 0f;
+                for (int i = 0; i < 50000; i++)
+                {
+                    value = math.exp10(math.sqrt(value));
+                }
+            }
+        }
+        
+        /* test 1
+        if (useJobs)
+        {
+            NativeList<JobHandle> jobHandleList = new NativeList<JobHandle>(Allocator.Temp);
+            for (int i = 0; i < 10; i++)
+            {
+                 JobHandle jobHandle = ReallyToughTaskJob();
+                 jobHandleList.Add(jobHandle);
+            }
+            JobHandle.CompleteAll(jobHandleList);
+            jobHandleList.Dispose();
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ReallyToughTask();
+            }
+        }*/
+       
+    //    Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000) + "ms");
+    }
+
+    private void ReallyToughTask()
+    {
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+
+    private JobHandle ReallyToughTaskJob()
+    {
+        ReallyToughJob job = new ReallyToughJob();
+        return job.Schedule();
+    }
+}
+
+[BurstCompile]
+public struct ReallyToughJob : IJob {
+    public void Execute() {
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
+
+[BurstCompile]
+public struct ReallyToughParallelJob: IJobParallelFor
+{
+    public NativeArray<float3> positionArray;
+    public NativeArray<float> moveYArray;
+    [ReadOnly] public float deltaTime;
+
+    public void Execute(int index)
+    {
+        positionArray[index] += new float3(0, moveYArray[index] * deltaTime, 0f);
+        if (positionArray[index].y > 5f)
+        {
+            moveYArray[index] = -math.abs(moveYArray[index]);
+        }
+        if (positionArray[index].y < -5f)
+        {
+            moveYArray[index] = +math.abs(moveYArray[index]);
+        }
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
+
+[BurstCompile]
+public struct ReallyToughParallelJobTransforms : IJobParallelForTransform
+{
+    public NativeArray<float> moveYArray;
+    [ReadOnly] public float deltaTime;
+
+    public void Execute(int index, TransformAccess transform)
+    {
+        transform.position += new Vector3(0, moveYArray[index] * deltaTime, 0f);
+        if (transform.position.y > 5f)
+        {
+            moveYArray[index] = -math.abs(moveYArray[index]);
+        }
+        if (transform.position.y < -5f)
+        {
+            moveYArray[index] = +math.abs(moveYArray[index]);
+        }
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
